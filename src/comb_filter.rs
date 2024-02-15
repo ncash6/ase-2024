3c370c2f8f935c0b357843b6b926e2571efb18dd
ncash6 <cashnic1@gmail.com>
2024-02-14 20:40:02 -0500

    Comb Filter Implemented

--- src/comb_filter.rs
+++ src/comb_filter.rs
@@ -1,5 +1,12 @@
 pub struct CombFilter {
     // TODO: your code here
+    delay_len: usize,
+    delay_line: Vec<Vec<f32>>,
+    filter_coefficients: Vec<f64>,
+    feedback: f32,
+    filter_type: FilterType,
+    sample_rate_hz: f32,
+    num_channels: usize,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -21,25 +28,114 @@ pub enum Error {
 
 impl CombFilter {
     pub fn new(filter_type: FilterType, max_delay_secs: f32, sample_rate_hz: f32, num_channels: usize) -> Self {
-        todo!("implement")
+        // Initialized variables for self
+        let delay_len = (max_delay_secs * sample_rate_hz) as usize;
+        let delay_line = vec![vec![0.0; delay_len]; num_channels];
+        let filter_coefficients = vec![0.0; delay_len];
+
+        CombFilter {
+            delay_len,
+            delay_line,
+            filter_coefficients,
+            sample_rate_hz,
+            num_channels,
+            feedback: 0.5, // default feedback value
+            filter_type,
+        }
     }
 
     pub fn reset(&mut self) {
-        todo!("implement")
+        // Zeroed delay line
+        for channel in &mut self.delay_line {
+            for sample in channel {
+                *sample = 0.0;
+            }
+        }
+        // Reset mutable states
+        self.feedback = 0.5;
     }
 
     pub fn process(&mut self, input: &[&[f32]], output: &mut [&mut [f32]]) {
-        todo!("implement");
+        match self.filter_type {
+            FilterType::FIR => {
+                // Matlab FIR Implementation
+                for (channel, & in_channel) in input.iter().enumerate() {
+                    let fir_delay_length = self.delay_line[channel].len();
+                    let filter_coeff_length = self.filter_coefficients.len();
+
+                    for (samp, &in_samp) in in_channel.iter().enumerate() {
+                        let mut out_del_sample = 0.0;
+
+                        // FIR difference equation
+                        for n in 0..filter_coeff_length {
+                            if samp >= n {
+                                out_del_sample += in_channel[samp - n] * (self.filter_coefficients[n] as f32);
+                            }
+                        }
+                        output[channel][samp] = out_del_sample;
+
+                        // delay line update
+                        let mut update_delay_line = vec![0.0; fir_delay_length];
+                        update_delay_line[0] = in_samp;
+                        update_delay_line[1..].copy_from_slice(&self.delay_line[channel][0..(fir_delay_length - 1)]);
+                        self.delay_line[channel] = update_delay_line;
+                    }
+                }
+            }
+            FilterType::IIR => {
+                // Matlab IIR Implementation
+                for (channel, & in_channel) in input.iter().enumerate() {
+                    let iir_delay_length = self.delay_line[channel].len();
+
+                    for (samp, &in_samp) in in_channel.iter().enumerate() {
+                        // IIR difference equation
+                        let out_del_sample = self.delay_line[channel][iir_delay_length - 1] * self.feedback;
+                        output[channel][samp] = in_samp + out_del_sample;
+
+                        // delay line update
+                        let mut update_delay_line = vec![0.0; iir_delay_length];
+                        update_delay_line[0] = output[channel][samp];
+                        update_delay_line[1..].copy_from_slice(&self.delay_line[channel][0..(iir_delay_length - 1)]);
+                        self.delay_line[channel] = update_delay_line;
+                    }
+                }
+            }   
+        }
+        todo!();
     }
 
     pub fn set_param(&mut self, param: FilterParam, value: f32) -> Result<(), Error> {
-        todo!("implement")
+        // Optional parameters are gain and delay
+        match param {
+            FilterParam::Gain => {
+                if value <= 0.0 {
+                    return Err(Error::InvalidValue {param,value});
+                }
+
+                self.feedback = value; 
+            }
+            FilterParam::Delay => {
+                if value <= 0.0 {
+                    return Err(Error::InvalidValue {param,value});
+                }
+                self.delay_len = (value * self.sample_rate_hz) as usize;
+            }   
+        }
+        Ok(())
     }
 
     pub fn get_param(&self, param: FilterParam) -> f32 {
-        todo!("implement")
+        // Optional parameters are gain and delay
+        match param {
+            FilterParam::Gain => {
+                self.feedback
+            }
+            FilterParam::Delay => {
+                self.delay_line.len() as f32
+            }
+        }
     }
-
+    
     // TODO: feel free to define other functions for your own use
 }
 
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     // Parse command line arguments
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
+    if args.len() < 5 {
         eprintln!("Usage: {} <input wave filename> <output text filename>", args[0]);
         return
     }
@@ -20,11 +20,100 @@ fn main() {
     // Open the input wave file
     let mut reader = hound::WavReader::open(&args[1]).unwrap();
     let spec = reader.spec();
-    let channels = spec.channels;
+    let channels = spec.channels; // number of channels in input file
 
     // TODO: Modify this to process audio in blocks using your comb filter and write the result to an audio file.
-    //       Use the following block size:
+
+    // Define block size
     let block_size = 1024;
+
+    // Open output wave file
+    let mut writer = hound::WavWriter::create(&args[2], spec).unwrap();
+
+    // Assign sample rate from input
+    let sample_rate = spec.sample_rate as f32; 
+
+    // Command line comb filter argument (FIR or IIR)
+    let comb_filter_type = match args[3].as_str() { 
+        "FIR" => comb_filter::FilterType::FIR,
+        "IIR" => comb_filter::FilterType::IIR,
+        _ => {
+            eprintln!("Invalid filter type. Acceptable options as FIR or IIR.");
+            return;
+        }
+    };
+
+    // Command line filter parameter type (Gain or Delay)
+    let comb_filter_parameter = match args[4].as_str() {
+        "Gain" => comb_filter::FilterParam::Gain,
+        "Delay" => comb_filter::FilterParam::Delay,
+        _ => {
+            eprintln!("Invalid filter type. Acceptable options as Gain or Delay.");
+            return;
+        }
+        // Ok(delay) => comb_filter::FilterParam::Delay,
+        // Err(_) => {
+        //     eprintln!("Invalid max delay value. Floating point number required.");
+        //     return;
+        // }
+    }; 
+
+    // Command line filter parameter argument 
+    let comb_filter_value: f32 = match args[5].parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Invalid effect parameter value. Floating point number required.");
+            return;
+        }
+    }; 
+
+    // Set max_delay_sec
+    let mut max_delay = 0.0;
+
+    if let comb_filter::FilterParam::Delay = comb_filter_parameter {
+        max_delay = comb_filter_value;
+    }
+
+    // Instantiate CombFilter
+    let mut comb_filter = comb_filter::CombFilter::new(comb_filter_type, max_delay, sample_rate, channels as usize);
+    
+
+    // Store wave file audio samples in buffer
+    let mut in_buffer =  vec![0.0; block_size * channels as usize];
+    let mut out_buffer = vec![0.0; block_size * channels as usize];
+    
+    while let Some(samp_read) = reader.samples::<i16>().next() {
+        let samp: i16 = match samp_read {
+            Ok(samp) => samp,
+            Err(err) => {
+                eprintln!("Error reading samples: {}", err);
+                return;
+            }   
+        };
+
+        // Normalization
+        for n in 0..block_size {
+            in_buffer[n] = samp as f32 / std::i16::MAX as f32;
+        }
+
+        // Block audio and process using comb filter
+        for chnl in 0..channels {
+            let start_indx = chnl as usize * block_size;
+            let end_indx = (chnl as usize + 1) * block_size;
+            let in_chnl = &in_buffer[start_indx..end_indx];
+            let out_chnl = &mut out_buffer[start_indx..end_indx];
+            comb_filter.process(&[in_chnl], &mut[out_chnl]);
+        }
+
+        // Write processed block to wave file
+        for ob in &out_buffer {
+            let out_samp = (*ob * std::i16::MAX as f32) as i16; // Denormalization
+            if let Err(err)= writer.write_sample(out_samp) {
+                eprintln!("Error wrinting sample to wave file: {}", err);
+                return;
+            }
+        }
+    }
 
     // Read audio data and write it to the output text file (one column per channel)
     let mut out = File::create(&args[2]).expect("Unable to create file");
