0c6c3568fb91420b4fb4cf8ffaebbcf2b5d571a4
ncash6 <cashnic1@gmail.com>
2024-04-13 19:52:23 -0400

    Added frequency domain convolution

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -1,4 +1,7 @@
 use crate::ring_buffer::RingBuffer;
+use rustfft::FftPlanner;
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
 
 struct FastConvolver {
     n_ir: usize,
@@ -25,6 +28,8 @@ impl FastConvolver {
     }
 
     pub fn reset(&mut self) {
+        // Note: The way I have things setup now means none of the self variables are changed to be reset
+        // Reset may be useful for the flush function.
         todo!("implement")
     }
 
@@ -57,12 +62,78 @@ impl FastConvolver {
                         current_sum += signal[i] * ring_buffer.get(combined_len - i - 1);
                     }
 
-                    output[k] = current_sum // Convolution results stored in output buffer
+                    // Convolution results stored in output buffer
+                    output[k] = current_sum 
                 }
             }
             ConvolutionMode::FrequencyDomain { block_size } => {
                 // Partitioned Fast Convolution Frequency Domain
                 
+                let n_input = input.len();
+                let combined_len = n_input + self.n_ir - 1; // Common Length between signal and impulse
+
+                // Initialized impulse response ring buffer
+                let mut ring_buffer = RingBuffer::new(combined_len);
+
+                // Push Impulse Response into RingBuffer
+                for &val in self.impulse_response.iter() {
+                    ring_buffer.push(val);
+                }
+
+                // Initialized output index
+                let mut index_output = 0;
+
+                for start_blk in (0..n_input).step_by(block_size) {
+                    let end_blk = std::cmp::min(start_blk + self.n_ir- 1, n_input);
+                    let blk_len= end_blk - start_blk;
+
+                    // Zero pad input block to combined length
+                    let mut signal_blk = vec![Complex::zero(); combined_len];
+                    for (i, &val) in input[start_blk..end_blk].iter().enumerate() {
+                        signal_blk[i] = Complex::new(val, 0.0);
+                    }
+
+                    // FFT on input signal
+                    let mut fft_plan = FftPlanner::new();
+                    let fft = fft_plan.plan_fft(combined_len, rustfft::FftDirection::Forward);
+                    fft.process(&mut signal_blk);
+
+                    // Zero pad impulse reponse block to combined length
+                    let mut ir_blk = vec![Complex::zero(); combined_len];
+                    for i in 0..combined_len {
+                        ir_blk[i] = Complex::new(ring_buffer.get(i), 0.0);
+                    }
+
+                    // FFT on impulse response
+                    let fft = fft_plan.plan_fft(combined_len, rustfft::FftDirection::Forward);
+                    fft.process(&mut ir_blk);
+
+                    // Element-wise multiplication (convolution) in frequency domain
+                    for i in 0..combined_len {
+                        signal_blk[i] *= ir_blk[i];
+                    }
+
+                    // Return to time domain
+                    let mut ifft_plan = FftPlanner::new();
+                    let ifft = ifft_plan.plan_fft(combined_len, rustfft::FftDirection::Inverse);
+                    ifft.process(&mut signal_blk);
+
+                    // Convolution results stored in output buffer
+                    for k in 0..blk_len {
+                        output[index_output + 1] = signal_blk[k].re;
+                    }
+
+                    // Shift ring buffer header by block length
+                    for _ in 0..blk_len {
+                        ring_buffer.pop();
+                    }
+                    for &val in &input[start_blk..end_blk] {
+                        ring_buffer.push(val);
+                    }
+
+                    // Updated ouput index 
+                    index_output += blk_len;
+                }
             }
         }
     }
@@ -71,7 +142,6 @@ impl FastConvolver {
         todo!("implement")
     }
 
-    // TODO: feel free to define other functions for your own use
 }
 
 // TODO: add tests
